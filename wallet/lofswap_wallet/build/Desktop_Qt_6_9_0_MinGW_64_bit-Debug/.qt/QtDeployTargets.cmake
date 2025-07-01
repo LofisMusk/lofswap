ab43877408c152753fd0c82a9716c578252e0e7d
LofisMusk <106832291+LofisMusk@users.noreply.github.com>
2025-07-01 23:20:57 +0200

    Delete wallet/lofswap_wallet directory

--- wallet/lofswap_wallet/build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_lofswap_wallet_FILE Z:/lofswap/wallet/lofswap_wallet/build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/lofswap_wallet.exe)
-set(__QT_DEPLOY_TARGET_lofswap_wallet_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_lofswap_wallet_RUNTIME_DLLS C:/Qt/6.9.0/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.9.0/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.9.0/mingw_64/bin/Qt6Core.dll)
