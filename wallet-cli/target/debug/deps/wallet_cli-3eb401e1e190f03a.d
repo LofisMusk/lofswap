b0980668fcde7728ae5010e952cb5fd023f8f526
LofisMusk <106832291+LofisMusk@users.noreply.github.com>
2025-07-01 23:24:51 +0200

    Delete wallet-cli/target directory

--- wallet-cli/target/debug/deps/wallet_cli-3eb401e1e190f03a.d
+++
@@ -1,5 +0,0 @@
-C:\Users\mekamb\Desktop\blockchain\wallet-cli\target\debug\deps\wallet_cli-3eb401e1e190f03a.d: src\main.rs
-
-C:\Users\mekamb\Desktop\blockchain\wallet-cli\target\debug\deps\libwallet_cli-3eb401e1e190f03a.rmeta: src\main.rs
-
-src\main.rs:
--- wallet-cli/target/debug/deps/wallet_cli-fc1d1e02d32318cf.d
+++
@@ -1,5 +0,0 @@
-C:\Users\mekamb\Desktop\blockchain\wallet-cli\target\debug\deps\wallet_cli-fc1d1e02d32318cf.d: src\main.rs
-
-C:\Users\mekamb\Desktop\blockchain\wallet-cli\target\debug\deps\libwallet_cli-fc1d1e02d32318cf.rmeta: src\main.rs
-
-src\main.rs:
