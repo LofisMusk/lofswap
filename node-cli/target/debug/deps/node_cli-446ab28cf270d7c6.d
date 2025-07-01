4a775189462bc5377f7fb9686e45f1919155c85e
LofisMusk <106832291+LofisMusk@users.noreply.github.com>
2025-07-01 23:25:01 +0200

    Delete node-cli/target directory

--- node-cli/target/debug/deps/node_cli-446ab28cf270d7c6.d
+++
@@ -1,5 +0,0 @@
-C:\Users\mekamb\Desktop\blockchain\node-cli\target\debug\deps\node_cli-446ab28cf270d7c6.d: src\main.rs
-
-C:\Users\mekamb\Desktop\blockchain\node-cli\target\debug\deps\libnode_cli-446ab28cf270d7c6.rmeta: src\main.rs
-
-src\main.rs:
